d53d5b06b57669bdd2b4af585eec6e53212562e8
Roland Liu <rolandleou@gmail.com>
2022-08-14 18:24:07 +0800

    Initial E-commerence spring boot project

---
+++ src/main/java/com/rolandleou/skymall/SkymallApplication.java
@@ -0,0 +1,13 @@
+package com.rolandleou.skymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkymallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkymallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rolandleou/skymall/SkymallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolandleou.skymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkymallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
