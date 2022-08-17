54c821f3801b5eb70b0e26e1e65adef3ea0fc249
Roland Liu <rolandleou@gmail.com>
2022-08-17 14:59:44 +0800

    商品功能 - 實作查詢所有商品列表 (排序)

--- src/main/java/com/rolandleou/skymall/controller/ProductController.java
+++ src/main/java/com/rolandleou/skymall/controller/ProductController.java
@@ -81,11 +81,20 @@ public class ProductController {
 	
 	
 	@GetMapping("/products")
-	public ResponseEntity<List<Product>> getProducts(@RequestParam(required = false) ProductCategory category,
-													@RequestParam(required = false) String search) {
+	public ResponseEntity<List<Product>> getProducts(
+			// filtering 查詢條件
+			@RequestParam(required = false) ProductCategory category,
+			@RequestParam(required = false) String search,
+			
+			// Sorting 排序
+			@RequestParam(defaultValue = "created_date") String orderBy,
+			@RequestParam(defaultValue = "desc") String sort
+			) {
 		ProductQueryParams productQueryParams = new ProductQueryParams();
 		productQueryParams.setCategory(category);
 		productQueryParams.setSearch(search);
+		productQueryParams.setOrderBy(orderBy);
+		productQueryParams.setSort(sort);
 		
 		List<Product> productList = productService.getProducts(productQueryParams);
 		
