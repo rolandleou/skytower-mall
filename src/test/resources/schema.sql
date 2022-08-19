b61b41801f1ff22b73243d3f5b52b34f20d4e2c2
Roland Liu <rolandleou@gmail.com>
2022-08-19 13:53:45 +0800

    帳號功能 - 單元測試

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,3 +1,4 @@
+-- product
 CREATE TABLE IF NOT EXISTS product
 (
     product_id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -7,6 +8,16 @@ CREATE TABLE IF NOT EXISTS product
     price              INT          NOT NULL,
     stock              INT          NOT NULL,
     description        VARCHAR(1024),
+    created_date       TIMESTAMP    NOT NULL,
+    last_modified_date TIMESTAMP    NOT NULL
+    );
+    
+    -- user
+CREATE TABLE IF NOT EXISTS user
+(
+    user_id            INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email              VARCHAR(256) NOT NULL UNIQUE,
+    password           VARCHAR(256) NOT NULL,
     created_date       TIMESTAMP    NOT NULL,
     last_modified_date TIMESTAMP    NOT NULL
     );
